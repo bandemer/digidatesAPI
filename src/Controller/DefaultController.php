5f378bc3cee3982227a29d6934f116c427dbfa89
Matthias Bandemer <git@bandemer.net>
2020-05-09 23:39:54 +0200

    Add more functions

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,18 +9,57 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/unixtime", name="unixtime")
+     * UnixTime
+     *
+     * @Route("/unixtime/{timestamp}", name="unixtime", methods={"GET"})
      */
-    public function unixTime()
+    public function unixtime(string $timestamp = '')
     {
-        return $this->json(['time' => time()]);
+        $unixTime = time();
+        if ($timestamp != '') {
+
+            $unixTime = strtotime($timestamp);
+        }
+
+        return $this->json(['time' => $unixTime]);
     }
 
     /**
-     * @Route("/kw", name="kw")
+     * Kalenderwoche
+     *
+     * @Route("/week/{timestamp}", name="week", methods={"GET"})
      */
-    public function kw()
+    public function week(string $timestamp = '')
     {
-        return $this->json(['kw' => date('W')]);
+        $unixTime = time();
+        if ($timestamp != '') {
+
+            $unixTime = strtotime($timestamp);
+        }
+
+        $kw = (int) date('W', $unixTime);
+
+        return $this->json(['week' => $kw]);
     }
+
+    /**
+     * Schaltjahr
+     *
+     * @Route("/leapyear/{timestamp}", name="leapyear", methods={"GET"})
+     */
+    public function leapyear(string $timestamp = '')
+    {
+        $leapYear = false;
+        $unixTime = time();
+        if ($timestamp != '') {
+            $unixTime = strtotime($timestamp);
+        }
+
+        if (date('L', $unixTime) == '1') {
+            $leapYear = true;
+        }
+
+        return $this->json(['leapyear' => $leapYear]);
+    }
+
 }
