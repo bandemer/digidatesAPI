8335b551cd2b6f8cc2c21142793b29b92372b4e7
Matthias Bandemer <git@bandemer.net>
2023-11-04 21:38:04 +0100

    Add tracking of HTTP referrer

--- src/Controller/StatistikController.php
+++ src/Controller/StatistikController.php
@@ -11,27 +11,25 @@ use MatomoTracker;
 
 class StatistikController extends AbstractController
 {
-    #[Route(path: '/st/', name: 'statistik')]
+    #[Route(path: '/st', name: 'statistik')]
     public function statistik(Request $req, StatistikService $statistikService): JsonResponse
     {
         $pageUrl = $req->get('pu', '');
         $pageTitle = $req->get('pt', '');
+        $pageReferrer = $req->get('rf', '');
         $screenWidth = $req->get('sw', 0);
         $screenHeight = $req->get('sh', 0);
-
         if ($statistikService->getUrl() != 'https://' AND 
             $pageUrl != '' AND
             $pageTitle != '' AND $screenWidth > 0 AND $screenHeight > 0) {
-
             $matomoTracker = new MatomoTracker((int) $statistikService->getSiteid(), $statistikService->getUrl());
             $matomoTracker->setTokenAuth($statistikService->getToken());
             $matomoTracker->disableCookieSupport();
             $matomoTracker->setUrl($pageUrl);
+            $matomoTracker->setUrlReferrer($pageReferrer);
             $matomoTracker->setResolution($screenWidth, $screenHeight);
             $matomoTracker->doTrackPageView($pageTitle);
         }
-
         return new JsonResponse(['status' => 'OK']);
     }
-
 }
